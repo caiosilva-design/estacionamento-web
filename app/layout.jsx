d8840a2c3292c6d7cebf7921bb4e7852e3cf6c0b
caiosilva-design <caiodasilvacaio@gmail.com>
2026-03-20 19:43:42 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,14 +2,10 @@ export const metadata = {
  title: "Estacionamento",
  description: "Sistema de estacionamento",
 };
-export default function RootLayout({
- children,
-}: {
- children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
  return (
 <html lang="pt-BR">
-<body>{children}</body>
+<body style={{ margin: 0 }}>{children}</body>
 </html>
  );
 }
